dda8beeb16d78fe368fabd5fa24532939f8dd008
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-06-23 17:07:42 -0400

    feat: add fastify and fastify-websocket

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello World')
+import Fastify, { FastifyInstance } from 'fastify'
+import FastifyWebsocket from 'fastify-websocket'
+
+async function start() {
+  const server = Fastify({})
+
+  /**
+   * Register Plugins
+   */
+  server.register(FastifyWebsocket)
+
+  server.get('/', { websocket: true }, (connection, request) => {
+    connection.socket.on('message', message => {
+      connection.socket.send('Hi From Server')
+    })
+  })
+
+  server.listen(4000, (err) => {
+    if (err) {
+      server.log.error(err)
+      process.exit(1)
+    }
+  })
+}
+
+start()
