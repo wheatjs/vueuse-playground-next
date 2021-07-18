de34e9d4032958eeafe2d054aa357dca9f3379fe
wheatjs <jacobrclevenger@gmail.com>
2021-07-18 16:56:16 -0400

    fix: add cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,19 @@
 import fastify from 'fastify'
+import fastifyCors from 'fastify-cors'
 import { Server } from 'socket.io'
 import { registerRoutes } from './routes'
 import { registerSockets } from './sockets'
 
 export async function main() {
-  const app = fastify()
+  const app = fastify({
+
+  })
+
+  app.register(fastifyCors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT'],
+  })
+
   const io = new Server(app.server, {
     cors: {
       origin: '*',
